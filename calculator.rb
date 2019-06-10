2aac100c28c31a354d516c68c0516b644dbeacf7
Thomas Chilzer <tchilzer2@gmail.com>
2019-06-10 16:38:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,4 @@ second_number = 0.5
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-quot
+quotient = first_number / second_number
